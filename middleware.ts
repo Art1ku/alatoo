491fc7afe026d2c833690c250e44f9c0cb906fdc
Art1ku <alihaniskenderbekov@gmail.com>
2024-12-25 10:14:39 +0600

    lol

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,22 @@
-// middleware.ts
+
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
   const { cookies, nextUrl } = req;
-  const authToken = cookies.get('authToken'); // Проверка токена из cookies
+  const authToken = cookies.get('authToken'); 
 
-  // Если пользователь не авторизован и пытается зайти на главную
   if (!authToken && nextUrl.pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  // Если авторизован и пытается зайти на /login, перенаправляем на главную
   if (authToken && nextUrl.pathname === '/login') {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  return NextResponse.next(); // Продолжаем выполнение запроса
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/', '/login', '/((?!_next/static|_next/image|favicon.ico).*)'], // Применяется к корневым страницам
+  matcher: ['/', '/login', '/((?!_next/static|_next/image|favicon.ico).*)'], 
 };
--- src/components/MainPageComponents/MainPage.tsx
+++ src/components/MainPageComponents/MainPage.tsx
@@ -1,6 +1,8 @@
+// MainPage.tsx
 'use client';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import MainLeftNav from './MainPageComponents/ClassesLeftNavigation/MainLeftNav';
@@ -8,12 +10,15 @@ import ClassAboutInfo from './MainPageComponents/ClassAboutInformation/ClassAbou
 
 export default function MainPage() {
   const [selectedItem, setSelectedItem] = useState<string | null>(null); // Состояние для хранения выбранного элемента
+  const [token, setToken] = useState<string>(''); // Обновлено на string (пустая строка вместо null)
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('authToken');
+    const token = Cookies.get('accessToken'); // Получаем токен из cookies
     if (!token) {
       router.push('/login'); // Перенаправление на страницу входа, если токен отсутствует
+    } else {
+      setToken(token); // Устанавливаем токен в состояние
     }
   }, [router]);
 
@@ -25,8 +30,9 @@ export default function MainPage() {
   return (
     <>
       <div className="flex h-auto">
-        <MainLeftNav onSelect={handleSelectItem} />  {/* Передача функции в MainLeftNav */}
-        <ClassAboutInfo selectedItem={selectedItem} />  {/* Передача выбранного элемента в ClassAboutInfo */}
+        <MainLeftNav onSelect={handleSelectItem} /> {/* Передача функции в MainLeftNav */}
+        {/* Передаем оба selectedItem и token в ClassAboutInfo */}
+        <ClassAboutInfo selectedItem={selectedItem} token={token} />
       </div>
     </>
   );
